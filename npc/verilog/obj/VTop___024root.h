189146c14cf428f80ad7017c522ad166333fea9b
tracer-ysyx <tracer@ysyx.org>
2024-07-26 22:15:20 +0800

    > compile NEMU ysyx_22040000 张三 Linux hujun-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:15:20 up 26 min,  1 user,  load average: 0.43, 0.35, 0.27

---
+++ npc/verilog/obj/VTop___024root.h
@@ -0,0 +1,43 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design internal header
+// See VTop.h for the primary calling header
+
+#ifndef VERILATED_VTOP___024ROOT_H_
+#define VERILATED_VTOP___024ROOT_H_  // guard
+
+#include "verilated.h"
+
+class VTop__Syms;
+
+class VTop___024root final : public VerilatedModule {
+  public:
+
+    // DESIGN SPECIFIC STATE
+    VL_IN8(clock,0,0);
+    VL_IN8(reset,0,0);
+    VL_IN8(io_a,0,0);
+    VL_IN8(io_b,0,0);
+    VL_OUT8(io_f,0,0);
+    CData/*0:0*/ __VactContinue;
+    IData/*31:0*/ __VstlIterCount;
+    IData/*31:0*/ __VicoIterCount;
+    IData/*31:0*/ __VactIterCount;
+    VlTriggerVec<1> __VstlTriggered;
+    VlTriggerVec<1> __VicoTriggered;
+    VlTriggerVec<0> __VactTriggered;
+    VlTriggerVec<0> __VnbaTriggered;
+
+    // INTERNAL VARIABLES
+    VTop__Syms* const vlSymsp;
+
+    // CONSTRUCTORS
+    VTop___024root(VTop__Syms* symsp, const char* v__name);
+    ~VTop___024root();
+    VL_UNCOPYABLE(VTop___024root);
+
+    // INTERNAL METHODS
+    void __Vconfigure(bool first);
+} VL_ATTR_ALIGNED(VL_CACHE_LINE_BYTES);
+
+
+#endif  // guard
