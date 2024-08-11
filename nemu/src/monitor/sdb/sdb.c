3b692813074e813ff9112190bc252e66ced62ebf
tracer-ysyx <tracer@ysyx.org>
2024-08-11 12:22:14 +0800

    > compile NEMU ysyx_22040000 张三 Linux hujun-virtual-machine 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:22:14 up  1:44,  0 users,  load average: 0.42, 0.24, 0.19

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -23,6 +23,14 @@ static int is_batch_mode = false;
 
 void init_regex();
 void init_wp_pool();
+word_t paddr_read(paddr_t addr, int len);
+
+static int cmd_help(char *args);
+static int cmd_c(char *args);
+static int cmd_q(char *args);
+static int cmd_si(char *args);
+static int cmd_info(char *args);
+static int cmd_x(char *args);
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 static char* rl_gets() {
@@ -42,6 +50,50 @@ static char* rl_gets() {
   return line_read;
 }
 
+
+
+
+static struct {
+  const char *name;
+  const char *description;
+  int (*handler) (char *);
+} cmd_table [] = {
+  { "help", "Display information about all supported commands", cmd_help },
+  { "c", "Continue the execution of the program", cmd_c },
+  { "q", "Exit NEMU", cmd_q },
+ 
+  /* TODO: Add more commands */
+  { "si", "Step execute N instructions", cmd_si },  
+  { "info", "Print information", cmd_info }, 
+  { "x", "Examine memory", cmd_x },   
+
+};
+
+#define NR_CMD ARRLEN(cmd_table)
+
+static int cmd_help(char *args) {
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+  int i;
+
+  if (arg == NULL) {
+    /* no argument given */
+    for (i = 0; i < NR_CMD; i ++) {
+      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+    }
+  }
+  else {
+    for (i = 0; i < NR_CMD; i ++) {
+      if (strcmp(arg, cmd_table[i].name) == 0) {
+        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+        return 0;
+      }
+    }
+    printf("Unknown command '%s'\n", arg);
+  }
+  return 0;
+}
+
 static int cmd_c(char *args) {
   cpu_exec(-1);
   return 0;
@@ -51,8 +103,6 @@ static int cmd_c(char *args) {
 static int cmd_q(char *args) {
   return -1;
 }
-
-static int cmd_help(char *args);
 
 static int cmd_si(char *args) {
   int N = 1;  
@@ -81,44 +131,34 @@ static int cmd_info(char *args) {
   return 0;
 }
 
+static int cmd_x(char *args) {
+  // 获取参数
+  char *argN = strtok(args, " ");
+  char *argEXPR = strtok(NULL, " ");
 
-static struct {
-  const char *name;
-  const char *description;
-  int (*handler) (char *);
-} cmd_table [] = {
-  { "help", "Display information about all supported commands", cmd_help },
-  { "c", "Continue the execution of the program", cmd_c },
-  { "q", "Exit NEMU", cmd_q },
- 
-  /* TODO: Add more commands */
-  { "si", "Step execute N instructions", cmd_si },  
-  { "info", "Print information", cmd_info }, 
-
-};
-
-#define NR_CMD ARRLEN(cmd_table)
-
-static int cmd_help(char *args) {
-  /* extract the first argument */
-  char *arg = strtok(NULL, " ");
-  int i;
-
-  if (arg == NULL) {
-    /* no argument given */
-    for (i = 0; i < NR_CMD; i ++) {
-      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-    }
+  if (argN == NULL || argEXPR == NULL) {
+    printf("Usage: x <N> <EXPR>\n");
+    return 0;
   }
-  else {
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(arg, cmd_table[i].name) == 0) {
-        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-        return 0;
-      }
-    }
-    printf("Unknown command '%s'\n", arg);
+
+  // 解析参数
+  char *ptrN = NULL;
+  char *ptrEXPR = NULL;
+  word_t N = strtoul(argN, &ptrN, 10); 
+  word_t EXPR = strtoul(argEXPR, &ptrEXPR, 16); 
+
+  // 参数合法性检查
+  if (((argN + strlen(argN)) != ptrN) || ((argEXPR + strlen(argEXPR)) != ptrEXPR)) {
+    printf("Check your input cmd, args can not contain non-numeric letters!\n");
+    return 0;
   }
+
+  // 打印内存内容
+  for (int i = 0; i < N; i++) {
+    word_t paddr = EXPR + i * 8;
+    printf("0x%x:\t0x%016x\n", paddr, paddr_read(paddr, 8));
+  }
+  
   return 0;
 }
 
