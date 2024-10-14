bf79cb715837573a00de13a100df8c223dd2a8b2
tracer-ysyx <tracer@ysyx.org>
2024-10-14 19:49:30 +0800

    > compile NEMU ysyx_22040000 张三 Linux hujun-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:49:30 up  1:43,  1 user,  load average: 1.22, 0.52, 0.24

--- abstract-machine/am/build/riscv32-nemu/src/riscv/nemu/cte.d
+++
@@ -1,7 +0,0 @@
-/home/hujun/ysyx-workbench/abstract-machine/am/build/riscv32-nemu/src/riscv/nemu/cte.o: \
- /home/hujun/ysyx-workbench/abstract-machine/am/src/riscv/nemu/cte.c \
- /home/hujun/ysyx-workbench/abstract-machine/am/include/am.h \
- /home/hujun/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
- /home/hujun/ysyx-workbench/abstract-machine/am/include/amdev.h \
- /home/hujun/ysyx-workbench/abstract-machine/am/src/riscv/riscv.h \
- /home/hujun/ysyx-workbench/abstract-machine/klib/include/klib.h
--- abstract-machine/am/build/riscv32-nemu/src/riscv/nemu/trap.d
+++
@@ -1,2 +0,0 @@
-/home/hujun/ysyx-workbench/abstract-machine/am/build/riscv32-nemu/src/riscv/nemu/trap.o: \
- /home/hujun/ysyx-workbench/abstract-machine/am/src/riscv/nemu/trap.S
