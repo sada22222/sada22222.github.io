f3fbed896c6c3b4a8835d3e743707f78b2eb6409
sada22222 <2120048010@qq.com>
2024-10-14 19:44:21 +0800

    NJU-ProjectN/abstract-machine ics2023 initialized
    NJU-ProjectN/abstract-machine c383159000a2975dabde43f221deae3d54bc48b0 am,npc: add uart config function

--- npc/verilog/build/obj_dir/auto_bind.d
+++
@@ -1,8 +0,0 @@
-auto_bind.o: /home/hujun/ysyx-workbench/npc/verilog/build/auto_bind.cpp \
- /home/hujun/ysyx-workbench/nvboard/usr/include/nvboard.h \
- /home/hujun/ysyx-workbench/nvboard/usr/include/pins.h Vkeyboard.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h
--- npc/verilog/build/obj_dir/keyboard.d
+++
@@ -1,8 +0,0 @@
-keyboard.o: /home/hujun/ysyx-workbench/npc/verilog/csrc/keyboard.cpp \
- /home/hujun/ysyx-workbench/nvboard/usr/include/nvboard.h \
- /home/hujun/ysyx-workbench/nvboard/usr/include/pins.h Vkeyboard.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h
