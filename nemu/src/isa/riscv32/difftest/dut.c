ff8091b83ea73db53ed727330331d563351192b7
tracer-ysyx <tracer@ysyx.org>
2024-10-30 10:34:54 +0800

    > compile NEMU ysyx_22040000 张三 Linux hujun-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:34:54 up 29 min,  0 users,  load average: 0.35, 0.38, 0.29

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -19,24 +19,13 @@
 
 // nemu as dut, spike as ref
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  bool ans = true;
   for (int i = 0; i < ARRLEN(cpu.gpr); i ++) {
     if (cpu.gpr[i] == ref_r->gpr[i]) continue;
-    printf("gpr in %s: 0x%08x != 0x%08x\n", regs[i], cpu.gpr[i], ref_r->gpr[i]);
-    return false;
+    ans = false;
   }
-
-  if (cpu.pc != ref_r->pc) {
-    printf("pc: 0x%08x != 0x%08x\n", cpu.pc, ref_r->pc);
-    return false;
-  }
-
-  int csr_addr[] = {0x300, 0x305, 0x341, 0x342};
-  for (int i = 0; i < ARRLEN(csr_addr); i ++) {
-    if (cpu.csr[i] == ref_r->csr[i]) continue;
-    printf("csr in %s: 0x%08x != 0x%08x\n", csrs[i], cpu.csr[i], ref_r->csr[i]);
-    return false;
-  }
-  return true;
+  if (cpu.pc != ref_r->pc) ans = true;
+  return ans;
 }
 
 void isa_difftest_attach() {
--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -19,10 +19,8 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-  cpu.csr[mcause] = NO;
-  cpu.csr[mepc] = epc + 4;
 
-  return cpu.csr[mtvec];
+  return 0;
 }
 
 word_t isa_query_intr() {
