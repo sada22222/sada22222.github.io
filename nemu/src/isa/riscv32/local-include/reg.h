7ec80be6256ef3c6456ddf7b13e008c77140c1d7
tracer-ysyx <tracer@ysyx.org>
2024-10-30 10:23:34 +0800

    > compile NEMU ysyx_22040000 张三 Linux hujun-virtual-machine 6.8.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Sep 11 15:25:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:23:34 up 18 min,  0 users,  load average: 0.18, 0.20, 0.21

--- nemu/src/isa/riscv32/local-include/reg.h
+++ nemu/src/isa/riscv32/local-include/reg.h
@@ -13,21 +13,51 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
-#ifndef __RISCV_REG_H__
-#define __RISCV_REG_H__
+#ifndef __RISCV32_REG_H__
+#define __RISCV32_REG_H__
 
 #include <common.h>
+#include <isa.h>
+
+extern const char *regs[];
+extern const char *csrs[];
 
 static inline int check_reg_idx(int idx) {
-  IFDEF(CONFIG_RT_CHECK, assert(idx >= 0 && idx < MUXDEF(CONFIG_RVE, 16, 32)));
+  IFDEF(CONFIG_RT_CHECK, assert(idx >= 0 && idx < 32));
   return idx;
 }
 
-#define gpr(idx) (cpu.gpr[check_reg_idx(idx)])
+// static inline int check_csr_idx(word_t idx) {
+//   int csrs[4] = {
+//     0x300,
+//     0x305,
+//     0x341, 
+//     0x342
+//   };
+//   bool ok = 0;
+//   for (int i = 0; i < ARRLEN(csrc); i ++) {
+//     if (csrs[i] == idx) ok = 1;
+//   }
+//   IDDEF(CONFIG_RT_CHECK, assert(! ok));
+//   return idx;
+// }
 
-static inline const char* reg_name(int idx) {
+#define gpr(idx) cpu.gpr[check_reg_idx(idx)]
+#define csr(imm) cpu.csr[csr2idx(imm)]
+
+static inline const char* reg_name(int idx, int width) {
   extern const char* regs[];
   return regs[check_reg_idx(idx)];
+}
+
+static inline int csr2idx(word_t imm) {
+  switch (imm) {
+    case 0x300: return mstatus;
+    case 0x305: return mtvec;
+    case 0x341: return mepc;
+    case 0x342: return mcause;
+    default: panic("UNKNOWN CSR");
+  }
 }
 
 #endif
