858ebad043f52541b9fa9423ce388375f9123112
sada22222 <2120048010@qq.com>
2024-11-29 23:44:56 +0800

    soc

---
+++ nemu/build/obj-riscv32-nemu-interpreter-so/src/monitor/trace/ftrace.d
@@ -0,0 +1,22 @@
+cmd_/home/hujun/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter-so/src/monitor/trace/ftrace.o := unused
+
+source_/home/hujun/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter-so/src/monitor/trace/ftrace.o := src/monitor/trace/ftrace.c
+
+deps_/home/hujun/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter-so/src/monitor/trace/ftrace.o := \
+    $(wildcard include/config/ftrace.h) \
+  /home/hujun/ysyx-workbench/nemu/src/monitor/trace/trace.h \
+  /home/hujun/ysyx-workbench/nemu/include/memory/paddr.h \
+    $(wildcard include/config/mbase.h) \
+    $(wildcard include/config/msize.h) \
+    $(wildcard include/config/pc/reset/offset.h) \
+  /home/hujun/ysyx-workbench/nemu/include/common.h \
+    $(wildcard include/config/target/am.h) \
+    $(wildcard include/config/isa64.h) \
+  /home/hujun/ysyx-workbench/nemu/include/macro.h \
+  /home/hujun/ysyx-workbench/nemu/include/debug.h \
+  /home/hujun/ysyx-workbench/nemu/include/utils.h \
+    $(wildcard include/config/target/native/elf.h) \
+
+/home/hujun/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter-so/src/monitor/trace/ftrace.o: $(deps_/home/hujun/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter-so/src/monitor/trace/ftrace.o)
+
+$(deps_/home/hujun/ysyx-workbench/nemu/build/obj-riscv32-nemu-interpreter-so/src/monitor/trace/ftrace.o):
